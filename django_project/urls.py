b2182de922947c643feffd9ad27cd9af5cf8efaf
Jon Lee <39420293+4ka0@users.noreply.github.com>
2022-11-12 21:44:05 +0900

    Code cleanup

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
 from django.urls import path, include
+from django.urls import reverse_lazy
+from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 
 from allauth.account import views as allauth_views
-from django.contrib.auth.decorators import login_required
-from django.urls import reverse_lazy
 
 
 urlpatterns = [
