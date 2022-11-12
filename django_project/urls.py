e31544f5b55f42eeb7e0809deca5975089824406
Jon Lee <39420293+4ka0@users.noreply.github.com>
2022-11-12 21:39:54 +0900

    allauth PasswordChangeView bug fix

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,12 +1,33 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from allauth.account import views as allauth_views
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+
 
 urlpatterns = [
+
     # Django admin
     path('admin/', admin.site.urls),
+
     # User authentication
     path('accounts/', include('allauth.urls')),
+
+    # allauth's PasswordChangeView redirects to the same form upon success.
+    # This is a known issue. Below, the url for PasswordChangeView is overridden to specify
+    # the desired redirect url. This solution taken from the issue page:
+    # https://github.com/pennersr/django-allauth/issues/468#issuecomment-643840017
+    path(
+        'account/password/change/',
+        login_required(
+            allauth_views.PasswordChangeView.as_view(
+                success_url=reverse_lazy('home')
+            )
+        ),
+        name='account_change_password'
+    ),
+
     # Local apps
     path('', include('pages.urls')),
 ]
