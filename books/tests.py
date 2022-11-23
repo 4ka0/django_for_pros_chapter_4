ba92ce52d0125930822b7b0abf747fe032904684
Jon Lee <39420293+4ka0@users.noreply.github.com>
2022-11-23 15:38:22 +0900

    Test search view

--- books/tests.py
+++ books/tests.py
@@ -20,28 +20,41 @@ class BookTests(TestCase):
             codename="special_status",
         )
 
-        cls.book = Book.objects.create(
+        cls.book1 = Book.objects.create(
             title="Harry Potter",
             author="JK Rowling",
             price="25.00",
         )
 
+        cls.book2 = Book.objects.create(
+            title="Dirty Harry",
+            author="Bobby Davro",
+            price="30.00",
+        )
+
+        cls.book3 = Book.objects.create(
+            title="The Two Towers",
+            author="J. R. R. Tolkien",
+            price="45.00",
+        )
+
         cls.review = Review.objects.create(
-            book=cls.book,
+            book=cls.book1,
             author=cls.user,
             text="An excellent review!"
         )
 
     def test_book_object_creation(self):
-        self.assertEqual(f"{self.book.title}", "Harry Potter")
-        self.assertEqual(f"{self.book.author}", "JK Rowling")
-        self.assertEqual(f"{self.book.price}", "25.00")
+        self.assertEqual(f"{self.book1.title}", "Harry Potter")
+        self.assertEqual(f"{self.book1.author}", "JK Rowling")
+        self.assertEqual(f"{self.book1.price}", "25.00")
 
     def test_book_page_list_view_when_logged_in(self):
         self.client.login(email="review_user@email.com", password="testpass123")
         response = self.client.get(reverse("book_list"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Harry Potter")
+        self.assertTemplateUsed(response, "_base.html")
         self.assertTemplateUsed(response, "books/book_list.html")
 
     def test_book_page_list_view_when_not_logged_in(self):
@@ -60,10 +73,26 @@ class BookTests(TestCase):
     def test_book_detail_view_with_permissions(self):
         self.client.login(email="review_user@email.com", password="testpass123")
         self.user.user_permissions.add(self.special_permission)
-        response = self.client.get(self.book.get_absolute_url())
+        response = self.client.get(self.book1.get_absolute_url())
         no_response = self.client.get("/books/12345/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "Harry Potter")
         self.assertContains(response, "An excellent review!")
+        self.assertTemplateUsed(response, "_base.html")
         self.assertTemplateUsed(response, "books/book_detail.html")
+
+    def test_book_search(self):
+        url = "{url}?{filter}={value}".format(
+            url=reverse("search_results"),
+            filter="q",
+            value="Harry"
+        )
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.status_code, 400)
+        self.assertContains(response, "Harry Potter")
+        self.assertContains(response, "Dirty Harry")
+        self.assertNotContains(response, "The Two Towers")
+        self.assertTemplateUsed(response, "_base.html")
+        self.assertTemplateUsed(response, "books/search_results.html")
